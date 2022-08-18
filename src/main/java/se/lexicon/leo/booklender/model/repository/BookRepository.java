f55c2956808f36840d537e751c574ae13dcaf654
Leo Yoosefi <leoyoosefi@gmail.com>
2022-08-19 00:20:14 +0200

    LibraryUserRepository find by email BookRepository find by: reserved,available,title LoanRepository find by: userId, bookId, concluded + Query

--- src/main/java/se/lexicon/leo/booklender/model/repository/BookRepository.java
+++ src/main/java/se/lexicon/leo/booklender/model/repository/BookRepository.java
@@ -3,5 +3,15 @@ package se.lexicon.leo.booklender.model.repository;
 import org.springframework.data.repository.CrudRepository;
 import se.lexicon.leo.booklender.model.entity.Book;
 
+import java.util.List;
+
 public interface BookRepository extends CrudRepository<Book, Integer> {
+
+
+    List<Book> findByReserved(boolean reserved);
+
+    List<Book> findByAvailable(boolean available);
+
+    List<Book> findByTitle(String title);
+
 }
--- src/main/java/se/lexicon/leo/booklender/model/repository/LibraryUserRepository.java
+++ src/main/java/se/lexicon/leo/booklender/model/repository/LibraryUserRepository.java
@@ -3,5 +3,10 @@ package se.lexicon.leo.booklender.model.repository;
 import org.springframework.data.repository.CrudRepository;
 import se.lexicon.leo.booklender.model.entity.LibraryUser;
 
+import java.util.Optional;
+
 public interface LibraryUserRepository extends CrudRepository<LibraryUser, Integer> {
+
+    Optional<LibraryUser> findByEmail(String email);
+
 }
