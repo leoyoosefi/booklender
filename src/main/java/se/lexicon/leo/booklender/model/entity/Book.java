018848e99e4f3332a9698ecfb3ad6377c6b49019
Leo Yoosefi <leoyoosefi@gmail.com>
2022-08-18 13:04:48 +0200

    book entity completion

--- src/main/java/se/lexicon/leo/booklender/model/entity/Book.java
+++ src/main/java/se/lexicon/leo/booklender/model/entity/Book.java
@@ -1,39 +1,123 @@
 package se.lexicon.leo.booklender.model.entity;
-
-import lombok.*;
-import org.hibernate.Hibernate;
-
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import javax.persistence.*;
 import java.math.BigDecimal;
 
-@Setter
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode
 @ToString
+@EqualsAndHashCode
 
 @Entity
 @Table(name = "books")
-public class Book {
-
+public class Book
+{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int bookId;
+
     private String title;
-    private boolean available;
-    private boolean reserved;
+
+    private boolean available = true;
+
+    private boolean reserved = false;
+
     private int maxLoanDays;
+
+    private int additionalLoanDays = 0;
+
     private BigDecimal finePerDay;
+
     private String description;
 
 
+    public Book() {
+    }
 
-
-    public Book(String title, int maxLoanDays, BigDecimal finePerDay, String description) {
+    public Book(String title, int maxLoanDays, BigDecimal finePerDay, String description)
+    {
         this.title = title;
         this.maxLoanDays = maxLoanDays;
         this.finePerDay = finePerDay;
+        this.description = description;
+    }
+
+    public int getBookId()
+    {
+        return bookId;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public boolean isAvailable()
+    {
+        return available;
+    }
+
+    public void setAvailable(boolean available)
+    {
+        this.available = available;
+    }
+
+    public boolean isReserved()
+    {
+        return reserved;
+    }
+
+    public void setReserved(boolean reservedStatus)
+    {
+        this.reserved = reservedStatus;
+    }
+
+    public int getMaxLoanDays()
+    {
+        return maxLoanDays;
+    }
+
+    public void setMaxLoanDays(int maxLoanDays)
+    {
+        this.maxLoanDays = maxLoanDays;
+    }
+
+    public int getAdditionalLoanDays()
+    {
+        return additionalLoanDays;
+    }
+
+    public void setAdditionalLoanDays(int additionalLoanDays)
+    {
+        this.additionalLoanDays = additionalLoanDays;
+    }
+
+    public int getTotalLoanDays()
+    {
+        return maxLoanDays + additionalLoanDays;
+    }
+
+    public BigDecimal getFinePerDay()
+    {
+        return finePerDay;
+    }
+
+    public void setFinePerDay(BigDecimal finePerDay)
+    {
+        this.finePerDay = finePerDay;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
         this.description = description;
     }
 
