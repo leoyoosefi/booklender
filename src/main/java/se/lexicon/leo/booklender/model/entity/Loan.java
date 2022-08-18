59658603bc6abf6d9a8d1dcd31278c5dd2a484b8
Leo Yoosefi <leoyoosefi@gmail.com>
2022-08-18 14:57:59 +0200

    workshop part 1 completed

--- src/main/java/se/lexicon/leo/booklender/model/entity/Loan.java
+++ src/main/java/se/lexicon/leo/booklender/model/entity/Loan.java
@@ -3,74 +3,102 @@ package se.lexicon.leo.booklender.model.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.Period;
 
 
-@NoArgsConstructor
-@AllArgsConstructor
+
 @EqualsAndHashCode
 @ToString
 
 @Entity
 @Table(name = "loans")
-public class Loan {
-
+public class Loan
+{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long loanId;
-    @ManyToOne
+
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "loan_taker_id")
     private LibraryUser loanTaker;
-    @ManyToOne
+
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "book_book_id")
     private Book book;
-    private LocalDate loanDate;
-    private boolean concluded;
 
-    public Loan(LibraryUser loanTaker, Book book, LocalDate loanDate, boolean concluded) {
+    LocalDate loanDate;
+
+    boolean concluded;
+
+    public Loan() {
+    }
+
+    public Loan(LibraryUser loanTaker, Book book, LocalDate loanDate, boolean concluded)
+    {
         this.loanTaker = loanTaker;
         this.book = book;
         this.loanDate = loanDate;
         this.concluded = concluded;
     }
 
-    public long getLoanId() {
+    public long getLoanId()
+    {
         return loanId;
     }
 
-    public void setLoanId(long loanId) {
-        this.loanId = loanId;
-    }
-
-    public LibraryUser getLoanTaker() {
+    public LibraryUser getLoanTaker()
+    {
         return loanTaker;
     }
 
-    public void setLoanTaker(LibraryUser loanTaker) {
+    public void setLoanTaker(LibraryUser loanTaker)
+    {
         this.loanTaker = loanTaker;
     }
 
-    public Book getBook() {
+    public Book getBook()
+    {
         return book;
     }
 
-    public void setBook(Book book) {
+    public void setBook(Book book)
+    {
         this.book = book;
     }
 
-    public LocalDate getLoanDate() {
+    public boolean isOverdue()
+    {
+        return LocalDate.now().isAfter(getLoanDate().plusDays(book.getTotalLoanDays()));
+    }
+
+    public BigDecimal getFine()
+    {
+        return isOverdue() ? book.getFinePerDay().multiply(new BigDecimal(Period.between(getLoanDate(), LocalDate.now()).getDays())) : new BigDecimal(0);
+    }
+
+    public LocalDate getLoanDate()
+    {
         return loanDate;
     }
 
-    public void setLoanDate(LocalDate loanDate) {
-        this.loanDate = loanDate;
-    }
-
-    public boolean isConcluded() {
+    public boolean isConcluded()
+    {
         return concluded;
     }
 
-    public void setConcluded(boolean concluded) {
+    public void setConcluded(boolean concluded)
+    {
         this.concluded = concluded;
+    }
+
+    boolean extendLoan(int days)
+    {
+        if(book.isReserved() || days <= book.getAdditionalLoanDays())
+            return false;
+
+        book.setAdditionalLoanDays(days);
+        return true;
     }
 }
